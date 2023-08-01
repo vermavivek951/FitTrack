bf778c405a9a591ace258f7b7080d0a6d68e4332
vermavivek951 <vermavivek951@gmail.com>
2023-08-01 18:21:57 +0530

    First Commit

---
+++ angularapp/src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  API_KEY: 'PUT_API_KEY_HERE',
+  baseUrl: 'http://localhost:8080',
+};
---
+++ springapp/src/test/java/com/example/springapp/SpringappApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springapp;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
